46443eea7b401d4a78b8fc89bdd4bf7857dba005
Yorn <yornhamann@gmail.com>
2024-09-30 16:49:00 +0200

    commented script

--- Unity_Homework_1/Assets/+++Workdata/Scripts/Button script.cs
+++ Unity_Homework_1/Assets/+++Workdata/Scripts/Button script.cs
@@ -12,19 +12,26 @@ public class Buttonscript : MonoBehaviour
     [SerializeField] private GameObject panel2;
     void Start()
     {
+        //tells the buttons what methods to execute when starting the gmae
         panel1Button.onClick.AddListener(Panel2);
         panel2Button.onClick.AddListener(Panel1);
         
+        
+        //activates panel 1 and deactivates panel2
         panel2.SetActive(false);
         panel1.SetActive(true);
     }
 
+    
+    //this method activates Panel 1 and deactivates Panel 2
     void Panel1()
     {
         panel1.SetActive(true);
         panel2.SetActive(false);
     }
     
+    
+    //this method activates Panel 2 and deactivates Panel 1
     void Panel2()
     {
         panel1.SetActive(false);
